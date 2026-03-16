001ccd4a9dfaff699f449080558310659b3b7507
codewithwambi <wambielvis59@gmail.com>
2026-03-16 16:58:58 +0000

    models.py and admin,py code for suprvisions app

--- backend/config/views.py
+++ backend/config/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Internship System Backend is Running!</h1><p>Go to <a href='/admin/'>/admin/</a> to manage placements.</p>")
+    return HttpResponse("<h1>  hello team Internship System Backend is Running  smoothly!</h1><p>Go to <a href='/admin/'>/admin/</a> to manage placements.</p>")
